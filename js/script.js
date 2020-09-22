6d0323d940c00eca554fc5ec78603b6f15db5b00
Andrea Contestabile <andreacontest@gmail.com>
2020-09-22 16:13:55 +0200

    Ricerca e rendering risultati tramite barra di ricerca

--- js/script.js
+++ js/script.js
@@ -1,28 +1,62 @@
 $(document).ready(function() {
 
-  var search = "Lost";
-
+  // Inizializzazione template Handlebars
   var source = $("#movie-template").html();
   var template = Handlebars.compile(source);
 
- $.ajax(
-   {
-     "url": "https://api.themoviedb.org/3/search/movie",
-     "method": "GET",
-     "data": {
-       "api_key": "455a355d80afa37b1862eb26f4991d02",
-       "query": search,
-     },
-     "success": function(data) {
-       renderMovies(data.results);
-     },
-     "error": function() {
+  // Evento KEYPRESS (tasto INVIO) campo search
+  $("#search").keypress(function(e) {
+    // Se il tasto premuto è INVIO e il campo search non è vuoto
+    if ((e.which == 13) && ($("#search").val().length > 0)) {
+      // Salvo in una variabile il contenuto del campo search
+      var searchQuery = $("#search").val();
+      // Eseguo la funzione searchMovies passando come query di ricerca il contenuto del campo search
+      searchMovies(searchQuery);
+    }
+  });
 
+  // Evento CLICK sul bottone #search-btn
+  $("#search-btn").click(function() {
+    // Se il campo search non è vuoto
+    if ($("#search").val().length > 0) {
+      // Salvo in una variabile il contenuto del campo search
+      var searchQuery = $("#search").val();
+      // Eseguo la funzione searchMovies passando come query di ricerca il contenuto del campo search
+      searchMovies(searchQuery);
+    }
+  });
+
+
+  // Funzione searchMovies
+  function searchMovies(query) {
+    // Effettuo la chiamata ajax all'API TMDB.org
+    $.ajax(
+      {
+       "url": "https://api.themoviedb.org/3/search/movie",
+       "method": "GET",
+       "data": {
+         // Api Key
+         "api_key": "455a355d80afa37b1862eb26f4991d02",
+         // Query di ricerca
+         "query": query,
+       },
+       "success": function(data) {
+         // Al successo, eseguo la funzione renderMovies, passando come argomento l'array dei risultati
+         renderMovies(data.results);
+       },
+       "error": function() {
+         // All'errore, eseguo un alert di errore
+        alert("Errore di ricerca! Riprova.")
+       }
      }
-   }
- );
+   );
+ }
 
+ // Funzione renderMovies
  function renderMovies(results) {
+   // Svuoto la lista dei film ul.movie-list
+   $("ul.movie-list").empty();
+   // Effettuo un ciclo dell'array di risultati ottenuto dalla chiamata ajax
    for (var i = 0; i < results.length; i++) {
      // var context = {
      //   "title": results[i].title,
@@ -30,7 +64,9 @@ $(document).ready(function() {
      //   "original_language": results[i].original_language,
      //   "vote_average": results[i].vote_average
      // };
+     // Compilo il template Handlebars passando direttamente ogni oggetto/film (le chiavi corrispondono)
      var html = template(results[i]);
+     // Faccio l'append del template compilato all'interno della lista dei film
      $("ul.movie-list").append(html);
    }
  }
